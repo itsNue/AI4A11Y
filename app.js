0dc6d316e527fd449cdcf1ac594593c6484aa6a5
Uvindu Harshana <uvinduharshana@zohomail.com>
2023-08-07 11:10:02 +0530

    feat:tab switch fix

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const tabs = document.querySelectorAll('[data-tab-value]')
-const tabInfos = document.querySelectorAll('[data-tab-info]')
+function setupTabs() {
+  document.querySelectorAll(".tab-button").forEach((button) => {
+    button.addEventListener("click", () => {
+      const buttonContainer = button.parentElement;
+      const tabContainer = buttonContainer.parentElement;
+      const tabName = button.dataset.forTab;
+      const tabToActivate = tabContainer.querySelector(
+        `.tab-content[data-tab="${tabName}"]`
+      );
 
-tabs.forEach(tab => {
-    tab.addEventListener('click',()=> {
-        const target = document.querySelector(tab.dataset.tabValue)
-        tabInfos.forEach(tabInfo => {
-            tabInfo.classList.remove('active')
-        })
-        target.classList.add('active')
-    })
-})
+      buttonContainer.querySelectorAll(".tab-button").forEach((button) => {
+        button.classList.remove("tab-button-active");
+      });
+
+      tabContainer.querySelectorAll(".tab-content").forEach((button) => {
+        button.classList.remove("tab-content-active");
+      });
+
+      button.classList.add("tab-button-active");
+      tabToActivate.classList.add("tab-content-active");
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setupTabs();
+});
